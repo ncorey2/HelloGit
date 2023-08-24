d5d75e347d4e4ca26c9b5173468aa54a343050fe
ncorey2 <ncorey2@huskers.unl.edu>
2023-08-24 15:00:47 -0500

    Initial Commit

---
+++ src/Hello.java
@@ -0,0 +1,12 @@
+/**
+ * Author: Noah Corey (ncorey2@huskers.unl.edu) 
+ * Date: 08/24/2023
+ * 
+ *
+ */
+public class Hello {
+
+	public static void main(String args[]) {
+		System.out.println("Noah Corey");
+	}
+}
