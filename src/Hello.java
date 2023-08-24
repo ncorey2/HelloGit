48291ce8cd168463047b23cb35eb1c0edba5ddfc
ncorey2 <ncorey2@huskers.unl.edu>
2023-08-24 15:29:29 -0500

    Changed C to Java in the description

--- src/Hello.java
+++ src/Hello.java
@@ -2,7 +2,7 @@
  * Author: Noah Corey (ncorey2@huskers.unl.edu) 
  * Date: 08/24/2023
  * 
- * A simple code in C that states my name and college major
+ * A simple code in Java that states my name and college major
  */
 public class Hello {
 
